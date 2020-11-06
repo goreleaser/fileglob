573e2529a4ab79e23d07fe389354787f65f7c530
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-11-06 18:53:00 -0300

    feat: use afero
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- glob.go
+++ glob.go
@@ -2,19 +2,23 @@ package zglob
 
 import (
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/gobwas/glob"
+	"github.com/spf13/afero"
 )
 
 func Glob(pattern string) ([]string, error) {
+	return GlobWithFs(afero.NewOsFs(), pattern)
+}
+
+func GlobWithFs(fs afero.Fs, pattern string) ([]string, error) {
 	var matches []string
 	g, err := glob.Compile(strings.TrimPrefix(pattern, "./"), '/')
 	if err != nil {
 		return matches, err
 	}
-	return matches, filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+	return matches, afero.Walk(fs, ".", func(path string, info os.FileInfo, err error) error {
  		if g.Match(path) {
  			matches = append(matches, path)
  		}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/goreleaser/zglob
 
 go 1.15
 
-require github.com/gobwas/glob v0.2.3
+require (
+	github.com/gobwas/glob v0.2.3
+	github.com/spf13/afero v1.4.1
+)

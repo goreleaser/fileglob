5f4934affb64372c477495d7e786c2be6144aa0a
Erik Geiser <erik.geiser@posteo.net>
2020-11-06 22:48:43 +0100

    feat: Add staticPrefix function.

---
+++ prefix.go
@@ -0,0 +1,55 @@
+package zglob
+
+import (
+	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/gobwas/glob/syntax/ast"
+	"github.com/gobwas/glob/syntax/lexer"
+)
+
+func staticPrefix(pattern string) (string, error) {
+	parts := strings.Split(pattern, string(filepath.Separator))
+
+	prefix := ""
+	if len(pattern) > 0 && pattern[0] == filepath.Separator {
+		prefix = string(filepath.Separator)
+	}
+
+	for _, part := range parts {
+		if part == "" {
+			continue
+		}
+
+		rootNode, err := ast.Parse(lexer.NewLexer(part))
+		if err != nil {
+			return "", fmt.Errorf("parse glob pattern: %w", err)
+		}
+
+		nChildren := len(rootNode.Children)
+
+		if nChildren > 1 {
+			break
+		}
+
+		candidate := rootNode
+		if len(rootNode.Children) == 1 {
+			candidate = rootNode.Children[0]
+		}
+
+		v, ok := candidate.Value.(ast.Text)
+		if !ok {
+			break
+		}
+
+		prefix = path.Join(prefix, v.Text)
+	}
+
+	if prefix == "" {
+		prefix = "."
+	}
+
+	return prefix, nil
+}
---
+++ prefix_test.go
@@ -0,0 +1,29 @@
+package zglob
+
+import "testing"
+
+func TestStaticPrefix(t *testing.T) {
+	var testCases = []struct {
+		pattern string
+		prefix  string
+	}{
+		{"/foo/b*ar/baz", "/foo"},
+		{"foo/bar", "foo/bar"},
+		{"/foo/bar/{b,p}az", "/foo/bar"},
+		{"*/foo", "."},
+		{"./", "."},
+		{"fo\\*o/bar/b*z", "fo*o/bar"},
+		{"/\\{foo\\}/bar", "/{foo}/bar"},
+	}
+
+	for _, testCase := range testCases {
+		prefix, err := staticPrefix(testCase.pattern)
+		if err != nil {
+			t.Errorf("staticPrefix: %v", err)
+		}
+		if prefix != testCase.prefix {
+			t.Errorf("prefixPrefix returned %q instead of %q for pattern %q",
+				prefix, testCase.prefix, testCase.pattern)
+		}
+	}
+}

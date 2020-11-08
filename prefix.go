95758cf365e1174ac6f4d1aa4abc5b2df62ad3aa
Erik G <erik.geiser@posteo.net>
2020-11-08 23:48:12 +0100

    feat: Add ContainsMatchers and ValidPattern. (#7)
    * feat: Add ContainsMatchers and ValidPattern.
    
    * fix: Fix failing tests.

--- prefix.go
+++ prefix.go
@@ -9,6 +9,50 @@ import (
 	"github.com/gobwas/glob/syntax/lexer"
 )
 
+// ValidPattern determines whether a pattern is valid. It returns the parser
+// error if the pattern is invalid and nil otherwise.
+func ValidPattern(pattern string) error {
+	_, err := ast.Parse(lexer.NewLexer(pattern))
+	return err // nolint:wrapcheck
+}
+
+// ContainsMatchers determines whether the pattern contains any type of glob
+// matcher. It will also return false if the pattern is an invalid expression.
+func ContainsMatchers(pattern string) bool {
+	rootNode, err := ast.Parse(lexer.NewLexer(pattern))
+	if err != nil {
+		return false
+	}
+
+	containsMatchers, _ := containsMatchers(rootNode)
+	return containsMatchers
+}
+
+func containsMatchers(node *ast.Node) (result bool, staticText string) {
+	// nolint:exhaustive
+	switch node.Kind {
+	case ast.KindPattern:
+		text := ""
+
+		for _, child := range node.Children {
+			cm, staticText := containsMatchers(child)
+			if cm {
+				return true, ""
+			}
+
+			text += staticText
+		}
+
+		return false, text
+	case ast.KindText:
+		return false, node.Value.(ast.Text).Text
+	case ast.KindNothing:
+		return false, ""
+	default:
+		return true, ""
+	}
+}
+
 // staticPrefix returns the file path inside the pattern up
 // to the first path element that contains a wildcard.
 func staticPrefix(pattern string) (string, error) {
@@ -29,24 +73,12 @@ func staticPrefix(pattern string) (string, error) {
 			return "", fmt.Errorf("parse glob pattern: %w", err)
 		}
 
-		nChildren := len(rootNode.Children)
-
-		if nChildren > 1 {
-			// this pattern is not static
+		cm, staticText := containsMatchers(rootNode)
+		if cm {
 			break
 		}
 
-		candidate := rootNode
-		if len(rootNode.Children) == 1 {
-			candidate = rootNode.Children[0]
-		}
-
-		v, ok := candidate.Value.(ast.Text)
-		if !ok {
-			break
-		}
-
-		prefix = filepath.Join(prefix, v.Text)
+		prefix = filepath.Join(prefix, staticText)
 	}
 
 	if prefix == "" {

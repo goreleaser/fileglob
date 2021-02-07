ca90a079b0684faacf82bff712550592acfca5ae
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-02-07 11:35:11 -0300

    docs: godoc
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- glob.go
+++ glob.go
@@ -82,6 +82,7 @@ func toNixPath(path string) string {
 }
 
 // Glob returns all files that match the given pattern in the current directory.
+// If the given pattern indicates an absolute path, it will glob from `/`.
 func Glob(pattern string, opts ...OptFunc) ([]string, error) {
 	prefix := "./"
 	if strings.HasPrefix(pattern, stringSeparator) {
